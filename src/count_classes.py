6d7d99e40d0e040d87d9bb63c91a811b65a1f84d
Tiffany Timbers <tiffany.timbers@stat.ubc.ca>
2023-11-26 13:32:47 -0800

    added main function

--- src/count_classes.py
+++ src/count_classes.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
-# read in wisconsin breast cancer data
-data = pd.read_csv('data/cancer_train.csv', index=None)
+def main():
+    # read in wisconsin breast cancer data
+    data = pd.read_csv('data/cancer_train.csv', index=None)
 
-result = data.groupby('diagnosis').size().reset_index(name='count')
-result = result.rename(columns={'diagnosis': 'Class'})
+    result = data.groupby('diagnosis').size().reset_index(name='count')
+    result = result.rename(columns={'diagnosis': 'Class'})
 
-data.to_csv('results/cancer/class_count.csv', index=False)
+    data.to_csv('results/cancer/class_count.csv', index=False)
+
+if __name__ == "__main__":
+    main()
